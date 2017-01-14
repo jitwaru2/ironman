8d50e5edd096ee98284ed79c6c32e84f1127b9fb
Kiba Gateaux <kibagateaux@gmail.com>
2017-01-14 15:13:34 -0500

    init d3 files

---
+++ www-root/js/index.js
@@ -0,0 +1,16 @@
+
+
+//  NAME
+//  START DATE
+//  END DATE
+//  TAGS
+//  TYPE: BILL, AMENDMENT
+
+
+//graph for representing all tags
+d3.select("body").style("color", function(err, data){
+  let { type_id, name, date, time, tags, description } = data.entity_overview;
+
+
+
+});
